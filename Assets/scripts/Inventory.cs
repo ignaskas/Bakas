49db476f3ae3cda9cb4526aa80c42f97f8e863bf
Ignas <ignaskas1@gmail.com>
2021-02-03 18:00:03 +0200

    added invetory

---
+++ Assets/scripts/Inventory.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Main_Behaviour mainBehaviourRef;
+    public menuBehaviur menuBehaviurRef;
+    
+    private bool _key = true;
+    private bool _sword;
+    private bool _staff;
+    private bool _blaster;
+    private bool _flaskEmpty = true;
+    private bool _flaskFull;
+    public bool _coinPurse;
+    private bool _armor;
+
+    public Main_Behaviour MainBehaviour => mainBehaviourRef;
+    //Update GameStates to reflect the choice
+    public void InventoryCheck()
+    {
+        switch (MainBehaviour.PlayerPoint.name)
+            {
+                case "sword":
+                    _sword = !_sword;
+                    _key = !_key;
+                    UpdateInventory(_sword, menuBehaviurRef.sword);
+                    UpdateInventory(_key, menuBehaviurRef.key);
+                    break;
+                case "staff":
+                    _staff = !_staff;
+                    _key = !_key;
+                    UpdateInventory(_staff, menuBehaviurRef.staff);
+                    UpdateInventory(_key, menuBehaviurRef.key);
+                    break;
+                case "blaster":
+                    _blaster = !_blaster;
+                    _key = !_key;
+                    UpdateInventory(_blaster, menuBehaviurRef.blaster);
+                    UpdateInventory(_key, menuBehaviurRef.key);
+                    break;
+                case "forrestIntCheck":
+                    _flaskEmpty = !_flaskEmpty;
+                    _flaskFull = !_flaskFull;
+                    UpdateInventory(_flaskEmpty, menuBehaviurRef.flaskEmpty);
+                    UpdateInventory(_flaskFull, menuBehaviurRef.flaskFull);
+                    MainBehaviour.FlaskUpdate.StatCheck.AttributeReturn = _flaskFull;
+                    break;
+                case "jungle":
+                    _coinPurse = !_coinPurse;
+                    UpdateInventory(_coinPurse, menuBehaviurRef.coinPurse);
+                    MainBehaviour.CoinUpdate.StatCheck.AttributeReturn = _coinPurse;
+                    break;
+                case "BuyArmor":
+                    _coinPurse = !_coinPurse;
+                    _armor = !_armor;
+                    UpdateInventory(_coinPurse, menuBehaviurRef.coinPurse);
+                    UpdateInventory(_armor, menuBehaviurRef.armor);
+                    break;
+                case "StealArmor":
+                    _armor = !_armor;
+                    UpdateInventory(_armor, menuBehaviurRef.armor);
+                    break;
+                default:
+                
+                    break;
+            }
+    }
+//Show hide items based on Players Choice 
+    private void UpdateInventory(bool itemState, GameObject currentItem)
+    {
+        currentItem.SetActive(itemState);
+    }
+    
+}
---
+++ Assets/scripts/ToolTip.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTip : MonoBehaviour
+{
+    [SerializeField]
+    private Camera uiCamera;
+    
+    private bool _toolTipToggle;
+    private ItemButtons ItemButtonsObj { set; get; }
+    private GameObject ClickedButton;
+    public GameObject ToolTipTextArea;
+    
+    public GameObject key;
+    public GameObject sword;
+    public GameObject staff;
+    public GameObject blaster;
+    public GameObject flaskEmpty;
+    public GameObject flaskFull;
+    public GameObject coinPurse;
+    public GameObject armor;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D rayHit = Physics2D.Raycast(ray, Vector2.zero);
+            
+            // RaycastHit rayCastHit;
+            // Ray rayCast = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // Physics.Raycast(rayCast, out rayCastHit)
+            if (rayHit)
+            {
+                Debug.Log(GameObject.FindWithTag("KeyButton"));
+                if (rayHit.collider.CompareTag("ArmorButton"))
+                {
+                    ClickedButton = GameObject.FindWithTag("ArmorButton");
+                    ItemButtonsObj = new ItemButtons(armor, "this is a armor");
+                    ToolTipToggle(ItemButtonsObj, ClickedButton, _toolTipToggle);
+                }
+                else if (rayHit.collider.CompareTag("KeyButton"))
+                {
+                    Debug.Log("we got a press!");
+                    ClickedButton = GameObject.FindWithTag("KeyButton");
+                    ItemButtonsObj = new ItemButtons(key, "this is a Key");
+                    _toolTipToggle = !_toolTipToggle;
+                    ToolTipToggle(ItemButtonsObj, ClickedButton, _toolTipToggle);
+                }
+            }
+        }
+    }
+
+    public class ItemButtons
+    {
+        public GameObject _itemButtons;
+        public string _toolTipText;
+
+        public ItemButtons(GameObject itemButtons, string toolTipText)
+        {
+            this._itemButtons = itemButtons;
+            this._toolTipText = toolTipText;
+        }
+    }
+    
+    private void ToolTipToggle(ItemButtons currentItemToolTip, GameObject currentItemButton, bool toggle)
+    {
+        gameObject.SetActive(toggle);
+    }
+}
